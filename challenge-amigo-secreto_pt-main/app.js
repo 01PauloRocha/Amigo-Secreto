8a8123db95deb845737e7310fde8e6d4e7d236fc
Paulo Rocha <phf.rocha32@gmail.com>
2025-02-28 20:00:45 -0300

    Botão Adicionar Lista e alerta, já codificado.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,24 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// app.js
+
+let amigos = [];
+
+function adicionarAmigo() {
+  let amigoInput = document.getElementById("amigo");
+  let amigoNome = amigoInput.value.trim(); // Adicionado .trim() para remover espaços extras
+
+  if (amigoNome === "") {
+    alert("O campo está vazio e deve ser preenchido.");
+  } else {
+    amigos.push(amigoNome);
+    atualizarListaAmigos();
+    amigoInput.value = ""; // Limpa o input
+  }
+}
+
+function atualizarListaAmigos() {
+  const listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = ""; // Limpa a lista antes de atualizar
+  
+}
+
+  
