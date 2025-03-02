ec3acc25b36d2bda78afb60f3005d0f2d22827bb
Paulo Rocha <phf.rocha32@gmail.com>
2025-03-02 20:36:39 -0300

    Feito alteração no código Botão Adicionar e feito codificação Botão Sortear Amigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,24 +1,51 @@
-// app.js
-
 let amigos = [];
+let lista = document.getElementById('listaAmigos');
 
+// Função para adicionar amigos
 function adicionarAmigo() {
-  let amigoInput = document.getElementById("amigo");
-  let amigoNome = amigoInput.value.trim(); // Adicionado .trim() para remover espaços extras
+    let nome = document.querySelector('input').value;
 
-  if (amigoNome === "") {
-    alert("O campo está vazio e deve ser preenchido.");
-  } else {
-    amigos.push(amigoNome);
-    atualizarListaAmigos();
-    amigoInput.value = ""; // Limpa o input
-  }
+    if (nome == '') {
+        alert("Você não pode deixar o espaço vazio!");
+        return;
+    } else {
+        amigos.push(nome);
+        limparCampo();
+    }
+    atualizarLista();   
 }
 
-function atualizarListaAmigos() {
-  const listaAmigos = document.getElementById("listaAmigos");
-  listaAmigos.innerHTML = ""; // Limpa a lista antes de atualizar
-  
+// Função para limpar o campo
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
 }
 
-  
+// Função para atualizar a lista
+function atualizarLista() {
+    lista.innerHTML = ''; // Limpar a lista existente
+
+    amigos.forEach(amigo => { // forEach() é um método de array que executa uma função para cada elemento do array.
+        let item = document.createElement('li'); //cria um novo elemento HTML li (item de lista). Em resumo, esta linha cria um novo item de lista para cada amigo.
+        item.textContent = amigo; // item.textContent define o texto dentro do elemento li
+        lista.appendChild(item); //adiciona o elemento li (com o nome do amigo) como um filho do elemento lista. Em resumo, esta linha adiciona o item de lista à lista exibida na página.
+    });
+}
+
+// Função para sortear um amigo secreto
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Adicione pelo menos dois amigo para realizar o sorteio!');
+        return;
+    }
+
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)]; // esta linha de código seleciona um nome aleatório do array amigos e o armazena na variável amigoSorteado.
+    resultado.innerHTML = `<li>O amigo secreto é: ${amigoSorteado}</li>`;
+}
+
+// Função para limpar a lista e recomeçar o sorteio
+function limparLista() {
+    amigos = [];
+    atualizarLista();
+    resultado.innerHTML = '';
+}
